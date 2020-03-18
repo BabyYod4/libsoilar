879ad9239d6208e4b7e7562c684a107cfda6e803
BabyYod4 <razzlenator@protonmail.com>
2020-03-18 13:02:00 +0000

    changed rfm95w channel settings

--- drivers/rfm95w/rfm95w_settings.h
+++ drivers/rfm95w/rfm95w_settings.h
@@ -4,10 +4,9 @@
 #include "../../tools/data_types/byte_types.h"
 
 //Max size Lora Packet 
-#define RFM95W_MAX_PKT_LENGTH                     255 
-// Max instances of the Rfm95W objectsthat can be made
-#define RFM95_FXOSC                               32000000.0
-
+#define RFM95W_MAX_PKT_LENGTH                      255 
+#define RFM95W_FXOSC                               32000000.0
+#define RFM95W_FSTEP                               (RFM95_FXOSC / 524288)
 
 #define RFM95W_CODE_RATE_4_5                      0x01
 #define RFM95W_CODE_RATE_4_6                      0x02
@@ -29,22 +28,23 @@
 #define RFM95W_FREQ_868MHZ                        byte3_t{0xD9, 0x00, 0x00}
 #define RFM95W_FREQ_915MHZ                        byte3_t{0x16, 0xE0, 0x00}
 
-// 865.20 MHZ
-#define RFM95W_CHANNEL_868_10                     byte3_t{0xD8, 0x4C, 0xCD}
-// 865.50 MHZ
-#define RFM95W_CHANNEL_868_11                     byte3_t{0xD8, 0x60, 0x00}
-// 865.80 MHZ
-#define RFM95W_CHANNEL_868_12                     byte3_t{0xD8, 0x73, 0x33}
-// 866.10 MHZ
-#define RFM95W_CHANNEL_868_13                     byte3_t{0xD8, 0x86, 0x66}
-// 866.40 MHZ
-#define RFM95W_CHANNEL_868_14                     byte3_t{0xD8, 0x99, 0x9A}
-// 866.70 MHZ
-#define RFM95W_CHANNEL_868_15                     byte3_t{0xD8, 0xAC, 0xCD}
-// 867 MHZ
-#define RFM95W_CHANNEL_868_16                     byte3_t{0xD8, 0xC0, 0x00}
-// 868 MHZ
-#define RFM95W_CHANNEL_868_17                     RFM95W_FREQ_868MHZ
+// 875.8125 MHZ
+#define RFM95W_CHANNEL_868_1                     byte3_t{0xDA, 0xF4, 0x00}
+// 875.9375 MHZ
+#define RFM95W_CHANNEL_868_2                     byte3_t{0xDA, 0xFC, 0x00}
+// 876.0625 MHZ
+#define RFM95W_CHANNEL_868_3                     byte3_t{0xDB, 0x04, 0x00}
+// 876.1875 MHZ
+#define RFM95W_CHANNEL_868_4                     byte3_t{0xDB, 0x0C, 0x00}
+
+// 866.8125 MHZ
+#define RFM95W_CHANNEL_868_5                     byte3_t{0xD8, 0xB4, 0x00}
+// 866.9375 MHZ
+#define RFM95W_CHANNEL_868_6                     byte3_t{0xD8, 0xBC, 0x00}
+// 867.0625 MHZ
+#define RFM95W_CHANNEL_868_7                     byte3_t{0xD8, 0xC4, 0x00}
+// 867.1875 MHZ
+#define RFM95W_CHANNEL_868_8                     byte3_t{0xD8, 0xCC, 0x00}
 
 #define RFM95W_DYNAMIC_PKT_LENGTH                 0
 
@@ -62,5 +62,16 @@
 #define RFM95W_DEFAULT_SETTINGS                   RFM95W_DEFAULT_FREQ, RFM95W_DEFAULT_SIGNAL_BW, RFM95W_DEFAULT_PREAMBLE_LENGTH, RFM95W_DEFAULT_TX_POWER, RFM95W_DEFAULT_SF, RFM95W_DEFAULT_CODE_RATE, RFM95W_DEFAULT_PKT_LENGTH, RFM95W_DEFAULT_SYNC_WORD, RFM95W_DEFAULT_CRC_MODE, RFM95W_DEFAULT_LOW_POWER_RECIEV_MODE, RFM95W_DEFAULT_INVERT_IQ  
 
 const float RFM95W_BW_TAB[] = {7.8, 10.4, 15.6, 20.8, 31.25, 41.7, 62.5, 125, 250, 500};
+
+static byte3_t RFM95W_CALC_FEQ_FAST( long freq ){
+    byte3_t tmp;
+    uint64_t frf = ((uint64_t)freq << 19) / 32000000;
+
+    tmp.msb = (uint8_t)(frf >> 16);
+    tmp.mid = (uint8_t)(frf >> 8);
+    tmp.lsb = (uint8_t)(frf >> 0);
+    // printf( "%04x %04x %04x", tmp.msb, tmp.mid, tmp.lsb );
+    return tmp;
+};
 
 #endif //RFM95W_SETTINGS_H
